54e27632ce15fe8a95e1bfbc219e8dd8e496b22a
Annie <asyso@thecei-pea.org>
2016-02-23 20:02:59 -0500

    added elevateZoom Plugin

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,13 +1,12 @@
 $(document).on('ready page:load', function(){
     $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-});
-
-$(document).on('ready page:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
     $('.rated').raty({ path: '/assets',
       readOnly: true,
       score: function() {
         return $(this).attr('data-score');
       }
     });
+
+    $('.img-zoom').elevateZoom();
+
 });
